61916daaf5b1ead49a2f8a714a1567222813c6e5
QBoff <kiper.daniel2006@gmail.com>
2023-01-30 12:36:18 +0300

    start intro activity

--- app/src/main/java/com/example/greenhouse_app/Intro.kt
+++ app/src/main/java/com/example/greenhouse_app/Intro.kt
@@ -3,9 +3,9 @@ package com.example.greenhouse_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
