08a4af0ccba37bae22d6ae34f3b909759ea18f5c
Dany <86149558+QBoff@users.noreply.github.com>
2023-02-01 18:38:05 +0300

    change findViewById to binding set up

--- app/src/main/java/com/example/greenhouse_app/IntroPage.kt
+++ app/src/main/java/com/example/greenhouse_app/IntroPage.kt
@@ -1,17 +1,21 @@
 package com.example.greenhouse_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.greenhouse_app.databinding.ActivityIntroPageBinding
 
 class IntroPage : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro_page)
-        val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener {
-            val intent = Intent(this, LoginPage::class.java)
+        binding = ActivityIntroPageBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.signUp.setOnClickListener {
+            val intent = Intent(this, SignUpPage::class.java)
             startActivity(intent)
         }
     }
