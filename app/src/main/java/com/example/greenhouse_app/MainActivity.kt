63dfe3754421abe27d595086d868c5bf261a5c34
QBoff <kiper.daniel2006@gmail.com>
2023-02-09 12:22:42 +0300

    Реализовал перемещение по bottom navigation bar

--- app/src/main/java/com/example/greenhouse_app/MainActivity.kt
+++ app/src/main/java/com/example/greenhouse_app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.greenhouse_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.greenhouse_app.databinding.ActivityMainBinding
 import com.example.greenhouse_app.databinding.FragmentSettingsBinding
 import com.example.greenhouse_app.fragments.ChartsFragment
@@ -14,42 +15,33 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         val homeFragment = HomeFragment()
         val settingsFragment = SettingsFragment()
         val notificationFragment = NotificationFragment()
         val chartsFragment = ChartsFragment()
-        setCurrentFragment(settingsFragment)
+        replaceFragment(homeFragment)
+
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.nav_settings -> {
-                    setCurrentFragment(settingsFragment)
-                    true
-                }
-                R.id.nav_home -> {
-                    setCurrentFragment(homeFragment)
-                    true
-                }
-                R.id.nav_notification -> {
-                    setCurrentFragment(notificationFragment)
-                    true
-                }
-                R.id.nav_chart -> {
-                    setCurrentFragment(chartsFragment)
-                    true
-                }
-                else -> {
-                    false
-                }
+                R.id.nav_home -> replaceFragment(homeFragment)
+                R.id.nav_settings -> replaceFragment(settingsFragment)
+                R.id.nav_notification -> replaceFragment(notificationFragment)
+                R.id.nav_chart -> replaceFragment(chartsFragment)
             }
+            true
         }
+
     }
 
-    private fun setCurrentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragment)
-            commit()
-        }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flFragment, fragment)
+        fragmentTransaction.commit()
     }
+
 }
