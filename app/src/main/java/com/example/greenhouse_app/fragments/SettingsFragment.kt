675098898d0f49f6f76c13d38e4df76646f55768
QBoff <kiper.daniel2006@gmail.com>
2023-02-10 14:07:24 +0300

    добавил кнопку

--- app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.greenhouse_app.fragments
 
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import com.example.greenhouse_app.R
 import com.example.greenhouse_app.databinding.FragmentSettingsBinding
@@ -19,9 +22,14 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingsBinding.inflate(layoutInflater)
+        binding.saveChages.setOnClickListener {
+            Log.d("Deb", "Ok")
+            val bt = binding.boundaryTempValue.text
+            val ht = binding.boundaryHumValue.text
 
-        binding.boundaryTempValue.setOnClickListener {
-            Toast.makeText(context, "Все работает", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "$bt, $ht", Toast.LENGTH_SHORT).show()
         }
+
+
     }
 }
