a5bff7fd7c4210eb94131144a2d8c1ef1b6ddfb1
Dany <86149558+QBoff@users.noreply.github.com>
2023-02-13 12:20:13 +0300

    Доверстал страницу настроек, начата работа над get запросами

--- app/src/main/java/com/example/greenhouse_app/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/greenhouse_app/fragments/NotificationFragment.kt
@@ -1,60 +1,51 @@
 package com.example.greenhouse_app.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.greenhouse_app.R
+import com.example.greenhouse_app.databinding.FragmentNotificationBinding
+import com.example.greenhouse_app.databinding.FragmentSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotificationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var binding: FragmentNotificationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+    ): View {
+        binding = FragmentNotificationBinding.inflate(inflater, container, false)
+
+        binding.loadDtNt.setOnClickListener {
+            val tm = loadData("tempV")
+            val hm = loadData("humV")
+            println("$tm $hm")
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    fun saveData(key: String, value: String) {
+        val editor = sharedPreferences.edit()
+
+        editor.putString(key, value)
+
+        editor.apply()
+    }
+
+    fun loadData(key: String): String? {
+        return sharedPreferences.getString(key, "")
     }
 }
--- app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
@@ -1,35 +1,60 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.greenhouse_app.fragments
 
-import android.content.SharedPreferences.Editor
+
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import android.widget.Toast
-import com.example.greenhouse_app.R
 import com.example.greenhouse_app.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var sharedPreferences: SharedPreferences
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentSettingsBinding.inflate(layoutInflater)
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
         binding.saveChages.setOnClickListener {
-            Log.d("Deb", "Ok")
-            val bt = binding.boundaryTempValue.text
-            val ht = binding.boundaryHumValue.text
+            val tempV = binding.boundaryTempValue.text
+            val humV = binding.boundaryHumValue.text
 
-            Toast.makeText(requireContext(), "$bt, $ht", Toast.LENGTH_SHORT).show()
+            saveData("tempV", tempV.toString())
+            saveData("humV", humV.toString())
         }
 
+        binding.extremeControlBtn.setOnClickListener {
+            Toast.makeText(requireContext(),
+                "Вы включили режим экстренного управления", Toast.LENGTH_SHORT)
+                .show()
+        }
 
+        return binding.root
     }
+
+    fun saveData(key: String, value: String) {
+        val editor = sharedPreferences.edit()
+
+        editor.putString(key, value)
+
+        editor.apply()
+    }
+
+    fun loadData(key: String): String? {
+        return sharedPreferences.getString(key, "")
+    }
+
+
 }
