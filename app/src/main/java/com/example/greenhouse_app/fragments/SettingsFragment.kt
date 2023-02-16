51225f4e04f45bb91554572d3b410b3d62c6edc1
Dany <86149558+QBoff@users.noreply.github.com>
2023-02-16 08:43:24 +0300

    Merge branch 'master' of D:\MyProjects\Mobile\Android\greenhouse-app with conflicts.

--- app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/greenhouse_app/fragments/SettingsFragment.kt
@@ -1,60 +1,35 @@
-@file:Suppress("DEPRECATION")
-
 package com.example.greenhouse_app.fragments
 
-
-import android.content.Context
-import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+import com.example.greenhouse_app.R
 import com.example.greenhouse_app.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
-    private lateinit var sharedPreferences: SharedPreferences
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
         binding.saveChages.setOnClickListener {
-            val tempV = binding.boundaryTempValue.text
-            val humV = binding.boundaryHumValue.text
+            Log.d("Deb", "Ok")
+            val bt = binding.boundaryTempValue.text
+            val ht = binding.boundaryHumValue.text
 
-            saveData("tempV", tempV.toString())
-            saveData("humV", humV.toString())
+            Toast.makeText(requireContext(), "$bt, $ht", Toast.LENGTH_SHORT).show()
         }
 
-        binding.extremeControlBtn.setOnClickListener {
-            Toast.makeText(requireContext(),
-                "Вы включили режим экстренного управления", Toast.LENGTH_SHORT)
-                .show()
-        }
 
-        return binding.root
     }
-
-    fun saveData(key: String, value: String) {
-        val editor = sharedPreferences.edit()
-
-        editor.putString(key, value)
-
-        editor.apply()
-    }
-
-    fun loadData(key: String): String? {
-        return sharedPreferences.getString(key, "")
-    }
-
-
 }
